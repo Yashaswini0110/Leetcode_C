a249f8832fd84ccd1e10b380ea92d9fde313af08
YASHASWINI R <167261278+Yashaswini0110@users.noreply.github.com>
2025-11-13 16:00:58 +0530

    Time: 16 ms (25.55%) | Memory: 19.8 MB (24.93%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,20 @@
-from typing import List
+from collections import defaultdict
 
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # 1. Count frequencies
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        # 1) Count frequency of each number
         count = {}
         for n in nums:
             count[n] = 1 + count.get(n, 0)
 
-        # 2. Create buckets
+        # 2) Create buckets: index = frequency, value = list of numbers
         freq = [[] for _ in range(len(nums) + 1)]
         for n, c in count.items():
             freq[c].append(n)
 
-        # 3. Collect results
+        # 3) Go from highest frequency to lowest, pick elements
         res = []
-        for i in range(len(freq) - 1, 0, -1):  # from high freq to low
+        for i in range(len(freq) - 1, 0, -1):   # from n down to 1
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
