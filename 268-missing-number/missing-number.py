25c36a81ff560189f4e52805b8d5e75e5c172a3a
YASHASWINI R <167261278+Yashaswini0110@users.noreply.github.com>
2025-11-23 06:45:54 +0530

    Time: 4 ms (47.45%) | Memory: 18.6 MB (92.17%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
-        es=(n*(n+1))//2
-        ass=sum(nums)
-        return es-ass
+        xor_all=0
+        for i in range (n+1):
+            xor_all ^=i
+        for num in nums:
+            xor_all^=num
+        return xor_all
+                
