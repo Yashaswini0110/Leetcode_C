a9031f0a0de8c0dab186b1266243cd21109644ce
YASHASWINI R <167261278+Yashaswini0110@users.noreply.github.com>
2026-01-02 09:58:38 +0530

    Time: 7 ms (5.10%) | Memory: 17.5 MB (83.58%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,28 +1,41 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        i = 0
-        n = len(s)
-        INT_MIN = -2**31            # -2147483648
-        INT_MAX = 2**31 - 1
-        while i< n and s[i]==" ":
-            i+=1
-        if i ==n:
-            return 0
-        sign=1
-        if s[i] == "-":
-            i+=1
-            sign=-1
-        elif s[i]=="+":
-            i+=1
-        num=0
-        while i<n and s[i].isdigit():
-            digit=ord(s[i])- ord("0")
+        INT_MAX=2**31-1
+        INT_MIN= -2**31
+
+        def helper (i, sign, result, started):
+            if i ==len(s):
+                return sign*result
+            ch=s[i]
             
-            if num > (INT_MAX -digit) //10:
-                if sign==1:
-                    return INT_MAX
+            # 0-> nothing seen yet
+            if  started==0 and ch==' ':
+                return helper(i+1,sign, result, started)
+            # 0-> sign allow once
+            if started==0 and (ch=="+" or ch=="-"):
+                if ch=='-':
+                    sign=-1
                 else :
-                    return INT_MIN
-            num=num*10+digit
-            i+=1
-        return num*sign
+                    sign=1
+                started =1
+                return helper(i+1, sign,result,started)
+            # digit allowed only if not finished 
+            if ch.isdigit() :
+                started =2
+                digit=ord(ch)-ord('0')
+
+                if result >INT_MAX //10 or (result == INT_MAX // 10 and digit>INT_MAX%10):
+                    if sign==1:
+                        return INT_MAX
+                    else:
+                        return INT_MIN
+                result=result * 10 +digit 
+                return helper (i+1, sign, result, started)
+            if started==2:
+                return sign*result
+            return 0
+        return helper (0,1,0,0)
+
+
+        
+        
